6bf0189e56bf67da28102747c249984cd7783ca8
Abraham Morales Arroyo <abrahamof606@gmail.com>
2024-01-02 14:25:19 -0600

    More exercises from codecademy

---
+++ hideandseek/src/App.java
@@ -0,0 +1,12 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Let's play hide and seek");
+        System.out.print( "three...");
+
+        System.out.print( "two...");
+        System.out.println( "One...");
+        System.out.println( "Ready or not here I come!");
+
+        
+    }
+}
